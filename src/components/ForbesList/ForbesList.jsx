62f8d0be814ce809698cf1fa110efedd8b639986
OLEKSANDR KUROCHKIN <kurochkinac@gmail.com>
2023-07-29 17:09:20 +0300

    Task 3-4

--- src/components/ForbesList/ForbesList.jsx
+++ src/components/ForbesList/ForbesList.jsx
@@ -8,6 +8,29 @@ import {
   LeaderBoardProfiles,
 } from './ForbesList.styled';
 
-export const ForbesList = () => {
-  return <div>ForbesList</div>;
+export const ForbesList = ({ list }) => {
+  return (
+    <LeaderBoard>
+      <BoardHeader>
+        <BoardTitle>
+          <TitleTop>Forbes</TitleTop>
+          <TitleBottom>Leader board</TitleBottom>
+        </BoardTitle>
+      </BoardHeader>
+
+      <LeaderBoardProfiles>
+        {list.map(({ avatar, name, capital, isIncrease, id }) => {
+          return (
+            <ForbesListItem
+              key={id}
+              avatar={avatar}
+              name={name}
+              capital={capital}
+              isIncrease={isIncrease}
+            />
+          );
+        })}
+      </LeaderBoardProfiles>
+    </LeaderBoard>
+  );
 };
--- src/components/Service/PostingDatePeriod.js
+++ src/components/Service/PostingDatePeriod.js
@@ -1,7 +1,10 @@
-
-import { formatDistanceToNow } from 'date-fns'
+import { formatDistanceToNow, format } from 'date-fns'
 
 export const PostingDatePeriod = (periodTillNow) => {
   return formatDistanceToNow(
         new Date(periodTillNow)
 )}
+
+export const HistoryDate = (date) => {
+  return format(new Date(date), "Pp")
+}
